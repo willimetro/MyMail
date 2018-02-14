67e35ad22e02d0eb834d542be99939f2456573f9
willimetro <wilfredo.ugalde@gmail.com>
2018-02-14 00:52:39 -0300

    Se mejora la estructura del proyecto y se crea el fragment de lista de mails

---
+++ app/src/main/java/com/wugaldeq/udemy/mymail/activities/MyMailActivity.java
@@ -0,0 +1,15 @@
+package com.wugaldeq.udemy.mymail.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.wugaldeq.udemy.mymail.R;
+
+public class MyMailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_mail);
+    }
+}
